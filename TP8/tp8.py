4e4a5b583eab6fabe59ec3237361642108770573
Narek Davtyan <narek.davtyan@etu.univ-grenoble-alpes.fr>
2018-03-24 00:47:33 +0100

    tp8 : bonus buggy

--- TP8/tp8.py
+++ TP8/tp8.py
@@ -102,7 +102,93 @@ def Subdivide( M0, u_closed, v_closed ) :
             M1[:,2*i+1,2*j+1] = (1.0/16.0)*(1.0*A +3.0*B +3.0*C +9.0*D)    
 
     return M1
-    
+
+def Sub_C_C( M0, u_closed, v_closed ) :
+
+    # dim is equal to 3
+    dim, m, n = M0.shape
+
+    # generate face points
+    M1 = np.zeros([dim, \
+        m if u_closed else (m-1), \
+        n if v_closed else (n-1)])
+    for i in range(0,m if u_closed else (m-1)) :
+        for j in range(0,n if v_closed else (n-1)) :
+
+            i_plus_one = (i+1)%m if u_closed else (i+1)
+            j_plus_one = (j+1)%n if v_closed else (j+1)
+            
+            A = M0[:,i,j]
+            B = M0[:,i_plus_one,j]
+            C = M0[:,i,j_plus_one]
+            D = M0[:,i_plus_one,j_plus_one]
+
+            M1[:,i+0,j+0] = (1.0/4.0)*(A + B + C + D)
+    print(M0)
+    print(M1)
+
+    # generate edge points
+    M2 = np.zeros([dim, \
+        m/2+m%2 if u_closed else (m/2+m%2-1), \
+        n/2+n%2 if v_closed else (n/2+n%2-1)])
+    M3 = np.zeros([dim, \
+        n/2+m%2 if v_closed else (n/2+n%2-1), \
+        m/2+n%2 if u_closed else (m/2+m%2-1)])
+    for i in range(0,m-1 if u_closed else (m-2)) :
+        for j in range(0,n-1 if v_closed else (n-2)) :
+            
+            i_plus_one = (i+1)%m if u_closed else (i+1)
+            j_plus_one = (j+1)%n if v_closed else (j+1)
+            i_plus_two = (i+2)%m if u_closed else (i+2)
+            j_plus_two = (j+2)%n if v_closed else (j+2)
+            
+            A = M0[:,i,j]
+            B = M0[:,i_plus_one,j]
+            C = M0[:,i,j_plus_one]
+            D = M0[:,i_plus_one,j_plus_one]
+            E = M0[:,i,j_plus_two]
+            F = M0[:,i_plus_one,j_plus_two]
+            G = M0[:,i_plus_two,j]
+            H = M0[:,i_plus_two,j_plus_one]
+
+            M2[:,i/2+i%2,j/2+j%2] = (1.0/4.0)*(A + B + 6.0*C + 6.0*D + E + F)
+            M3[:,i/2+i%2,j/2+j%2] = (1.0/4.0)*(A + C + 6.0*B + 6.0*D + G + H)
+
+    print(M2)
+    print(M3)
+
+    # modify the existing vertices
+    # (Q/n) + (2R/n) + (S(n-3)/n) , where valence n=4, hence (Q/4) + (R/2) + (S/4),
+    # with Q - the average of the surrounding face points, 
+    # R - the average of all surround edge midpoints, and 
+    # S - the control point
+    M4 = np.zeros([dim, \
+        n if v_closed else (n-1), \
+        m if u_closed else (m-1)])
+    for i in range(0,m-1 if u_closed else (m-2)) :
+        for j in range(0,n-1 if v_closed else (n-2)) :
+            
+            i_plus_one = (i+1)%m if u_closed else (i+1)
+            j_plus_one = (j+1)%n if v_closed else (j+1)
+
+            A = M1[:,i,j]
+            B = M1[:,i_plus_one,j]
+            C = M1[:,i,j_plus_one]
+            D = M1[:,i_plus_one,j_plus_one]
+            Q = (1.0/4.0)*(A + B + C + D)
+
+            i_plus_one = (i+1)%(m/2+m%2) if u_closed else (i+1)
+            j_plus_one = (j+1)%(n/2+n%2) if v_closed else (j+1)
+
+            E = M2[:,i,j]
+            F = M2[:,i_plus_one,j]
+            G = M3[:,i,j]
+            H = M3[:,i,j_plus_one]
+            R = (1.0/4.0)*(E + F + G + H)
+            S = M0[:,i_plus_one,j_plus_one]
+
+            M4[:,i,j] = (1.0/4.0)*Q + (1.0/2.0)*R + (1.0/4.0)*S
+            
 #-------------------------------------------------
 if __name__ == "__main__":
     
@@ -124,6 +210,13 @@ if __name__ == "__main__":
         depth = int(sys.argv[2])
     else :
         depth = 1
+
+    # arg 3 : algo choice
+    if len(sys.argv) > 3 :
+        # algo_cc=False
+        algo_cc = True
+    else :
+        algo_cc = False
 
     # filename
     filename = TP+"data/"+dataname+".net"
@@ -149,7 +242,8 @@ if __name__ == "__main__":
                 
         # iterative subdivision
         for d in range(depth) :
-            M = Subdivide(M,u_closed,v_closed)
+            Sub_C_C(M,u_closed,v_closed)
+            M = Subdivide(M,u_closed,v_closed) if not algo_cc else Sub_C_C(M,u_closed,v_closed)
         
         # u_closed : make rows periodic
         if u_closed :
